c58e946fa6d3aa133dc37eb5a3f40a44f0721b63
PATIENCE KANDARE <PATIENCE.KANDARE@HOTMAIL.COM>
2026-03-16 18:04:18 +0000

    add new post page

---
+++ src/app/posts/new/page.js
@@ -0,0 +1,32 @@
+import { db } from "@/utils/db"
+import { userAuth } from "@/utils/userAuth"
+import { redirect } from "next/navigation"
+
+export default async function NewPost() {
+    const userDatabase = await userAuth()
+
+    async function handleNewPost(formData) {
+        'use server'
+        const { content } = Object.fromEntries(formData)
+
+        const result = await db.query(`INSERT INTO opinion_posts (content) VALUES ($1) RETURNING id`,
+            [content]
+
+        )
+
+        redirect(`/posts/${result.rows[0].id}`)
+    }
+
+
+
+    return (
+        <div>
+            <h3>In my opinion...</h3>
+
+            <form action={handleNewPost}>
+            <textarea name="content" placeholder="Add yours here..."/>
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
