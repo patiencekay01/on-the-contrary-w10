4174c64ae7fb2699894b9789deb484aa49224f9f
PATIENCE KANDARE <PATIENCE.KANDARE@HOTMAIL.COM>
2026-03-17 01:14:39 +0000

    update user profile page

--- src/app/users/profile/page.js
+++ src/app/users/profile/page.js
@@ -5,10 +5,17 @@ export default async function UserProfile() {
     const userDatabase = await userAuth()
 
     return (
-        <div>
-            <h3>{userDatabase.username}</h3>
+       <main className="min-h-screen bg-neutral-100 flex items-center justify-center">
+        <div className="w-full max-w-5xl px-6">
+
+            <section className="rounded-xl bg-white p-8 shadow-sm border text-center">
+
+
+            <h3 className="text-3xl font-bold text-neutral-900">{userDatabase.username}</h3>
             <p>About me</p>
-            <p>{userDatabase.bio}</p>
+            <p className="mt-4 text-neutral-600">{userDatabase.bio}</p>
+        </section>
         </div>
+        </main>
     )
 }
